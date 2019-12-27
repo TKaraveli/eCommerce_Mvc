2a6b7f90de8a531f8deafd51e02ff4b315878f7e
TKaraveli <tayfunkaraveli@hotmail.com>
2019-12-27 05:51:13 +0300

    content güncellendi.

---
+++ eCommerce_Mvc.Services/LoginService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce_Mvc.Services
+{
+    public class LoginService
+    {
+        private static readonly string _username = "tayfun";
+        private static readonly string _password = "1234";
+
+        public bool IsExistLogin(string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(username) | string.IsNullOrWhiteSpace(password))
+                return false;
+
+            if (_username != username)
+                return false;
+
+            if (_password != password)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ eCommerce_Mvc/Models/HomeIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using eCommerce_Mvc.Entities.DTO;
+
+namespace eCommerce_Mvc.Models
+{
+    public class HomeIndexViewModel
+    {
+        public HomeIndexViewModel()
+        {
+            Products = new List<ProductDTO>();
+            Categories = new List<CategoryDTO>();
+        }
+
+        public List<ProductDTO> Products { get; set; }
+
+        public List<CategoryDTO> Categories { get; set; }
+    }
+}
