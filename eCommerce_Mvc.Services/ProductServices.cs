22f7518a60f84d260c84b8005defbac4442beef2
TKaraveli <tayfunkaraveli@hotmail.com>
2019-12-27 05:45:48 +0300

    services katmanı eklendi.

---
+++ eCommerce_Mvc.Services/ProductServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eCommerce_Mvc.DAL.Repository;
+using eCommerce_Mvc.Entities.DTO;
+using eCommerce_Mvc.Entities.Entity;
+using eCommerce_Mvc.Helpers.Extensions;
+
+namespace eCommerce_Mvc.Services
+{
+    public class ProductServices
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductServices()
+        {
+            _productRepository = new ProductRepository();
+        }
+
+        public List<ProductDTO> GetProductDTOs()
+        {
+            List<Product> productList = _productRepository.GetAllProducts();
+            List<ProductDTO> dtoProductList = productList.GetProductDto();
+
+            return dtoProductList;
+        }
+    }
+}
